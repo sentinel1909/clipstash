95f1d2026860280ded3f887e9453756c19a41f7d
Jeff Mitchell <sentinel1909@protonmail.com>
2022-10-01 23:02:59 -0700

    initial commit

---
+++ src/lib/domain/clip/field/mod.rs
@@ -0,0 +1,17 @@
+mod clip_id;
+mod content;
+mod expires;
+mod hits;
+mod password;
+mod posted;
+mod shortcode;
+mod title;
+
+pub use clip_id::ClipId;
+pub use content::Content;
+pub use expires::Expires;
+pub use hits::Hits;
+pub use password::Password;
+pub use posted::Posted;
+pub use shortcode::ShortCode;
+pub use title::Title;
---
+++ src/lib/domain/clip/mod.rs
@@ -0,0 +1,34 @@
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+pub mod field;
+
+#[derive(Debug, Error)]
+pub enum ClipError {
+    #[error("Invalid password: {0}")]
+    InvalidPassword(String),
+    #[error("Invalid title: {0}")]
+    InvalidTitle(String),
+    #[error("Empty content")]
+    EmptyContent,
+    #[error("Invalid date: {0}")]
+    InvalidDate(String),
+    #[error("Date parse error: {0}")]
+    DateParser(#[from] chrono::ParseError),
+    #[error("id parse error: {0}")]
+    Id(#[from] uuid::Error),
+    #[error("hits parse error: {0}")]
+    Hits(#[from] std::num::TryFromIntError),
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Clip {
+    pub clip_id: field::ClipId,
+    pub shortcode: field::ShortCode,
+    pub content: field::Content,
+    pub title: field::Title,
+    pub posted: field::Posted,
+    pub expires: field::Expires,
+    pub password: field::Password,
+    pub hits: field::Hits,
+}
